35c763276407ebbe1d62f4a376dc67c1c6e61762
ZechiuM <53609815+ZechiuM@users.noreply.github.com>
2020-07-28 10:01:12 +0100

    Converting sln to VS2017+

--- FA/FT/ACERIFT/AssemblyInfo.cs
+++ FA/FT/ACERIFT/AssemblyInfo.cs
@@ -1,18 +1,6 @@
-using System.Reflection;
+﻿using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
-
-// General Information about an assembly is controlled through the following
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
-[assembly: AssemblyTitle("IFT")]
-[assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Moody's Analytics")]
-[assembly: AssemblyProduct("RiskOrigins")]
-[assembly: AssemblyCopyright("Copyright © Moody's Analytics 2016")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
 
 // Setting ComVisible to false makes the types in this assembly not visible
 // to COM components.  If you need to access a type in this assembly from
@@ -21,17 +9,3 @@ using System.Runtime.InteropServices;
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("4aa15017-e3bf-4485-a7e8-d629a7b34598")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("6.5.8.0")]
-[assembly: AssemblyFileVersion("6.5.8.0")]
-[assembly: AssemblyInformationalVersion("6.5.8.0")]
